7528ab077fb498e1e086fd2eaee91d3b7829eca7
Neiman Marcus <136953008+NEIMAN-AI@users.noreply.github.com>
2023-07-31 11:11:05 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+from os import getenv as e
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+
+API_ID = e("API_ID", 13691707)
+API_HASH = e("API_HASH", "2a31b117896c5c7da27c74025aa602b8")
+
+BOT_TOKEN = e("BOT_TOKEN", None
